f1c5e73c8f20a56f646a1badb9ae2f5171ff78df
Jirka B <j.borovec+github@gmail.com>
2025-05-22 11:06:42 +0200

    make pkg installable

---
+++ src/fpdb/__init__.py
@@ -0,0 +1,15 @@
+from .forked_pdb import ForkedPdb
+
+# __version__ = "0.1.0"
+# __author__ = "Your Name"
+# __license__ = "Apache-2.0"
+# __status__ = "Development"
+# __description__ = "A Python package for debugging multi-processed code using PDB."
+# __keywords__ = ["debugging", "pdb", "multiprocessing"]
+# __long_description__ = """
+# ForkedPdb is a Python package that provides a subclass of the built-in PDB debugger,
+# allowing you to debug multi-processed code. It overrides the interaction method to
+# handle standard input correctly in a multi-process environment.
+# """
+# __long_description_content_type__ = "text/markdown"
+__all__ = ["ForkedPdb"]
