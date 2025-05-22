e8259a7f43050a33232ac3061a11fd5e75bd9596
Jirka B <j.borovec+github@gmail.com>
2025-05-22 12:42:54 +0200

    vesion

--- src/fpdb/__init__.py
+++ src/fpdb/__init__.py
@@ -1,11 +1,11 @@
 from .forked_pdb import ForkedPdb
 
-# __version__ = "0.1.0"
+__version__ = "0.0.1dev0"
 # __author__ = "Your Name"
-# __license__ = "Apache-2.0"
-# __status__ = "Development"
-# __description__ = "A Python package for debugging multi-processed code using PDB."
-# __keywords__ = ["debugging", "pdb", "multiprocessing"]
+__license__ = "Apache-2.0"
+__status__ = "Development"
+__description__ = "A Python package for debugging multi-processed code using PDB."
+__keywords__ = ["debugging", "pdb", "multiprocessing"]
 # __long_description__ = """
 # ForkedPdb is a Python package that provides a subclass of the built-in PDB debugger,
 # allowing you to debug multi-processed code. It overrides the interaction method to
